68e1e825f796941557b4f0752d330fa569e6d4b2
Mithridates__Hz <79919424+mehrdad-hz@users.noreply.github.com>
2023-06-15 14:09:58 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar />
+  <router-view />
 </template>
 
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: { Navbar },
+};
+</script>
+
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap");
+body {
+  background: #f2f2f2;
+  font-family: "Ubuntu", sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
 }
 </style>
